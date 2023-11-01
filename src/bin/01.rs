1631ae8e546673f4d2dc3b7122cf12180acd5f41
Kaleb Burris <kaleb.r.burris@gmail.com>
2023-11-01 00:04:34 -0800

    Remove tests from CI

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -21,19 +21,19 @@ pub fn part_one(input: &str) -> Option<u32> {
 
 pub fn part_two(input: &str) -> Option<u32> {
     let values = input
-    // Split each elf by double linebreak
-    .split("\n\n")
-    // Split each set of calories by a single linebreak
-    .map(
-        |x| {
-            x.split('\n')
-                // Parse out each calorie value
-                .map(|x| x.parse::<u32>().unwrap_or(0))
-                // Collect each value into a vec of u32s
-                .collect::<Vec<u32>>()
-        }, // Store each elf's calories in a vec of u32s
-    )
-    .collect::<Vec<Vec<u32>>>();
+        // Split each elf by double linebreak
+        .split("\n\n")
+        // Split each set of calories by a single linebreak
+        .map(
+            |x| {
+                x.split('\n')
+                    // Parse out each calorie value
+                    .map(|x| x.parse::<u32>().unwrap_or(0))
+                    // Collect each value into a vec of u32s
+                    .collect::<Vec<u32>>()
+            }, // Store each elf's calories in a vec of u32s
+        )
+        .collect::<Vec<Vec<u32>>>();
 
     // Sum and store the calories stored by each elf
     let mut elves = values.iter().map(|x| x.iter().sum()).collect::<Vec<u32>>();
@@ -44,20 +44,3 @@ pub fn part_two(input: &str) -> Option<u32> {
 }
 
 advent_of_code::main!(1);
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", 1));
-        assert_eq!(result, Some(69693));
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", 1));
-        assert_eq!(result, Some(11411));
-    }
-}
